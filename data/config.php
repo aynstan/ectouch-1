2c8f9692f77dabcc153e724fd1a3d0250b8513c9
高宗卫 <1009464715@qq.com>
2017-06-03 23:48:34 +0800

    初始化

---
+++ data/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'DB_TYPE'   => 'mysql',
+    'DB_HOST'   => 'localhost',
+    'DB_USER'   => 'test888',
+    'DB_PWD'   => 'test888',
+    'DB_NAME'   => 'test888',
+    'DB_PREFIX'   => 'ecs_',
+    'DB_PORT'   => '3306',
+    'DB_CHARSET'   => 'utf8',
+);
